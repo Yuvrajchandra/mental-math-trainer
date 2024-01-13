ffbb9782c7c2b6ace6818224ef972a1af8ac5c2f
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2024-01-13 18:35:07 +0530

    created empty NotFound component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom';
+import Trainer from './components/Trainer/Trainer.jsx';
+import NotFound from './components/NotFound/NotFound.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/trainer",
+    element: <Trainer />,
+  },
+  { path: "*", 
+    element: <NotFound />, 
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
